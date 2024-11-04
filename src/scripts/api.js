54354e9fc11375b2f21f71443cef74304c6a96cd
Breno Rocha <brenopenarocha2@gmail.com>
2024-11-04 08:54:00 -0300

    getWeatherData working with input value

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,9 +1,16 @@
-export default async function getWeatherData() {
-  let response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/florianopolis?unitGroup=metric&key=JKDS7FWSASJL9PLZSBU39ZSDE&contentType=json');
+async function getWeatherData(city) {
+
+  let apiKey = 'JKDS7FWSASJL9PLZSBU39ZSDE';
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`;
+  let response = await fetch(url);
   let weatherData = response.json();
 
+  console.log(city);
+  console.log(await weatherData);
   return weatherData;
 }
 
 let data = await getWeatherData();
 console.log(data.resolvedAddress);
+
+export { getWeatherData };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,6 @@
 import '../style.css';
+import { getWeatherData } from './api';
+let cityInput = document.querySelector('.search-input');
+let cityButton = document.querySelector('.search-submit');
+
+cityButton.addEventListener('click', () => getWeatherData(cityInput.value));
