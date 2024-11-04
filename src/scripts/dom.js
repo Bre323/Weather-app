86bfd7a7b27e92b1e18813de5b80f60c49c05fa7
Breno Rocha <brenopenarocha2@gmail.com>
2024-11-04 09:43:30 -0300

    displayWeatherInfo function with API data

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,44 +1,53 @@
 let weatherInfoSection = document.querySelector('.weather-info');
 let weekForecastSection = document.querySelector('.week-forecast');
 
-function displayWeatherInfo() {
+function displayWeatherInfo(weather) {
+  let cityResolvedAddress = weather.resolvedAddress;
+  let temperature = `${weather.currentConditions.temp}°C`;
+  let humidity = `${weather.currentConditions.humidity}%`;
+  let windSpeed = `${weather.currentConditions.windspeed}km/h`;
+  let chanceOfRain = `${weather.currentConditions.precipprob}%`;
+  let sunrise = weather.currentConditions.sunrise;
+  let moonphase = weather.currentConditions.moonphase;
+  let sunset = weather.currentConditions.sunset;
+  
   weatherInfoSection.innerHTML = '';
   weatherInfoSection.insertAdjacentHTML('beforeend', 
     `
       <div class="city">
-        <h2>Florianópolis, Brazil</h2>
-        <p class="temperature">27°C</p>
+        <h2>${cityResolvedAddress}</h2>
+        <p class="temperature">${temperature}</p>
       </div>
 
       <div class="info-grid">
         <div class="info">
           <p>Humidity</p>
-          <p>100%</p>
+          <p>${humidity}</p>
         </div>
 
         <div class="info">
           <p>Wind</p>
-          <p>100km/h</p>
+          <p>${windSpeed}</p>
         </div>
 
         <div class="info">
           <p>Chance of rain</p>
-          <p>100%</p>
+          <p>${chanceOfRain}</p>
         </div>
 
         <div class="info">
           <p>Sunrise</p>
-          <p>20:00</p>
+          <p>${sunrise}</p>
         </div>
 
         <div class="info">
           <p>Moon</p>
-          <p>100%</p>
+          <p>${moonphase}</p>
         </div>
 
         <div class="info">
           <p>Sunset</p>
-          <p>38:00</p>
+          <p>${sunset}</p>
         </div>
       </div>
     `
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,8 @@ import { displayWeatherInfo, displayWeekForecast } from './dom';
 let cityInput = document.querySelector('.search-input');
 let cityButton = document.querySelector('.search-submit');
 
-cityButton.addEventListener('click', () => {
-  getWeatherData(cityInput.value);
-  displayWeatherInfo();
+cityButton.addEventListener('click', async () => {
+  let weatherData = await getWeatherData(cityInput.value);
+  displayWeatherInfo(weatherData);
   displayWeekForecast();
 });
