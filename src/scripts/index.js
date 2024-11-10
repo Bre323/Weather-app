005b5c1eae43820f34a5a87a6f9e91a3ed40f1d2
Breno Rocha <brenopenarocha2@gmail.com>
2024-11-10 09:13:23 -0300

    Adding prevent default

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,10 @@ import { displayWeatherInfo, displayWeekForecast } from './dom';
 let cityInput = document.querySelector('.search-input');
 let cityButton = document.querySelector('.search-submit');
 
-cityButton.addEventListener('click', async () => {
+cityButton.addEventListener('click', async (event) => {
   let weatherData = await getWeatherData(cityInput.value);
+  event.preventDefault();
+  
   if(weatherData) {
     displayWeatherInfo(weatherData);
     displayWeekForecast(weatherData);
